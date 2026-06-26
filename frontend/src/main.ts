f7fc429ccd4945852d2f231923948a11813aded8
jesus rueda <jesus.r@nebula.com.co>
2026-06-25 22:51:35 -0500

    feat: register SearchInput component in main application

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -14,6 +14,7 @@ import Pagination from '@/components/page/Pagination.vue'
 import Page from '@/components/page/Page.vue'
 import Card from '@/components/page/Card.vue'
 import Heading from '@/components/page/Heading.vue'
+import SearchInput from '@/components/inputs/SearchInput.vue'
 
 import App from './App.vue'
 import router from './router'
@@ -41,6 +42,7 @@ app.component('Pagination', Pagination)
 app.component('Page', Page)
 app.component('Card', Card)
 app.component('Heading', Heading)
+app.component('SearchInput', SearchInput)
 
 app.use(createPinia())
 app.use(router)
