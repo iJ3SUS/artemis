08256a9e70960984d35eaeb7cd4f2b8161ba7386
jesus rueda <jesus.r@nebula.com.co>
2026-06-25 23:04:10 -0500

    refactor: streamline search functionality in employee query by removing unnecessary pipelines

--- backend/app/employees/routes/dashboard/browse.get.js
+++ backend/app/employees/routes/dashboard/browse.get.js
@@ -6,22 +6,16 @@ export const controller = async (req, rep) => {
 
     const { page, limit, search } = req.query
 
-    const pipelines = req.pipelines ?? []
-
-    if (search) {
-        pipelines.push({
-            $match: {
+    const result = await Employee.query()
+        .when(search, (q) => {
+            q.match({
                 $or: [
-                    { names: { $regex: search, $options: 'i' } },
-                    { surnames: { $regex: search, $options: 'i' } },
+                    { display_name: { $regex: search, $options: 'i' } },
                     { identification: { $regex: search, $options: 'i' } },
                     { email: { $regex: search, $options: 'i' } }
                 ]
-            }
+            })
         })
-    }
-
-    const result = await Employee.query(pipelines)
         .paginate(page || 1, limit || 10)
 
     return rep.send(result)
