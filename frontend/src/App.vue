6a158218005aaa8dc4f7198e59675e9758d0d281
jesus rueda <jesus.r@nebula.com.co>
2026-06-26 23:13:47 -0500

    refactor: remove page transition logic from App.vue and main.css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,6 @@
 <template>
     <Main>
-        <router-view v-slot="{ Component }">
-            <Transition name="page" mode="out-in">
-                <component :is="Component" />
-            </Transition>
-        </router-view>
+        <router-view />
     </Main>
 </template>
 
