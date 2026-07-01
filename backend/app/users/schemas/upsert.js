9df910ec626c6144fe3e99b8e02cf5a190bd87f5
jesus rueda <jesus.r@nebula.com.co>
2026-06-30 20:52:19 -0500

    feat: update phone and identification fields to allow null values and make them optional

--- backend/app/users/schemas/upsert.js
+++ backend/app/users/schemas/upsert.js
@@ -6,11 +6,11 @@ export default Joi.object({
     display_name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: Joi.object({
-        indicative: Joi.string().required(),
-        number: Joi.string().required()
-    }).required(),
+        indicative: Joi.string().allow(null, '').optional(),
+        number: Joi.string().allow(null, '').optional()
+    }).optional().allow(null),
     active: Joi.boolean().default(true),
-    identification: Joi.string().min(5).max(20).required(),
+    identification: Joi.string().allow(null, '').optional(),
     properties: Joi.object().pattern(Joi.string(), Joi.any()).default({}),
     roles: Joi.array().items(Joi.objectId()).default([])
 }).options({ stripUnknown: true })
